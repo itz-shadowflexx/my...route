b5943aa8d083d9e3ad6dec2b72d3f5be831ac316
itz-shadowflexx <pandaabhishek118@gmail.com>
2026-02-18 20:31:11 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+const routes = [
+  {
+    from: "PANITANKI",
+    to: "NJP ",
+    timing: "7:30 AM",
+    seats: 12
+  },
+  {
+    from: "NJP ",
+    to: "BAGDOGRA",
+    timing: "4:00 PM",
+    seats: 115
+  },
+  {
+    from : "MATIGARA",
+    to:"RANGAPANI",
+    timing: "6:00 pm",
+    seats: 9
+  },
+  {
+    from: "SHIVMANDIR ",
+    to: "NBMC&H",
+    timing: "9:00 AM",
+    seats: 6
+  }
+];
